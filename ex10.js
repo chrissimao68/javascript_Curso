4c32ab47b95c2d9ecf18005cc917eb98efb423bf
Christopher <chrissimao68@gmail.com>
2025-11-17 16:12:48 -0300

    frase

--- ex10.js
+++ ex10.js
@@ -1,1 +1,12 @@
+//Criar um array com 10 numeros e mostrar no array
+let prompt = require(`prompt-sync`)({ sigint: true });
+let numeros = [], num
+
+for (let i = 0; i < 10; i++) {
+    let num = Number(prompt(`Digite ${i + 1}º número: `));
+    numeros[i] = num
+
+}
+console.log(numeros)
+
 
