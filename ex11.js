e85eec9e30d9d8af7ac403030c06bfdd8bb243f4
Christopher <chrissimao68@gmail.com>
2025-11-17 17:08:44 -0300

    exeríicio email

--- ex11.js
+++ ex11.js
@@ -1,4 +1,4 @@
-let numeros
-for (let i = 0; i < 10; i++) {
-    console.log(i + 1)
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
 }
---
+++ ex12.js
@@ -0,0 +1,7 @@
+let prompt = require(`prompt-sync`)({ sigint: true });
+let email = [], dig
+for (let i = 0; i < 5; i++) {
+    let dig = String(prompt(`Digite ${i + 1}º email: `))
+    email[i] = dig
+}
+console.log(email)
