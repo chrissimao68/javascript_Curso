04f3ea0c35e972ba96bf430b5fc6bd7d589e27a5
Christopher <chrissimao68@gmail.com>
2025-11-18 16:47:41 -0300

    exemplo foreach

---
+++ exForeach.js
@@ -0,0 +1,10 @@
+//exemplo foreach
+let games = ["Minecraft", "Fortnite", "Roblox"];
+//Mostrar cada game com emoji
+games.forEach((game, index) => {
+    console.log(`${index + 1}. ${game}`);
+});
+
+games.forEach((elemento, indice) => {
+    //Ação para cada elemento
+});
