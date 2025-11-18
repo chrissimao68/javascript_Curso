7575bd910585ca05d0653df2b1f72f97cf502734
Christopher <chrissimao68@gmail.com>
2025-11-18 15:22:53 -0300

    exemplo push

---
+++ exPush.js
@@ -0,0 +1,9 @@
+let jogos = ["Minecraft", "Fortnite"];
+
+//Adicionar elementos no final
+jogos.push("Roblox");
+console.log(jogos);
+
+//Adicionar vários de uma vez
+jogos.push("GTA", "God of War");
+console.log(jogos)
