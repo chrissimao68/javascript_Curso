085616fadbec5deb74cc9c2f16c67b8a5f3c310a
Christopher <chrissimao68@gmail.com>
2025-11-19 16:00:33 -0300

    pedidos

---
+++ fix4.js
@@ -0,0 +1,14 @@
+let numeros = [5, 15, 8, 25, 3, 18]
+let contador = 0
+let i
+let numero
+
+numeros.forEach((numero) => {
+    if (numero > 10) {
+        console.log("Numeros maiores que 10: ", numero)
+    };
+    contador = contador + 1
+});
+
+
+
---
+++ fix5.js
@@ -0,0 +1,9 @@
+let pedidos = []
+pedidos.push("Coca Cola", "Maionese Caseira", "Amburger Artesanal", "Batata Rústica");
+console.log(pedidos);
+let removidos = pedidos.pop()
+console.log(removidos);
+let removidos2 = pedidos.pop()
+console.log(removidos2)
+
+console.log(pedidos)
