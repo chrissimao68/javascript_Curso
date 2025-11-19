4d4a8c7568f99dedd08780218aa2384c4b7c2d86
Christopher <chrissimao68@gmail.com>
2025-11-19 15:07:06 -0300

    lista

--- Fix1.js
+++ Fix1.js
@@ -1,7 +1,7 @@
 //Lista de compras, colocar no array e mostrar na tela
 let ListaCompras = []
 ListaCompras.push("Arroz", "Feijão", "Açucar", "Alcatra", "Maminha", "Fraudinha", "Óleo de Soja", "Queijo", "Alho", "Cebola")
-console.log(ListaCompras)
+console.log(ListaCompras);
 
 ListaCompras.forEach((Lista, index) => {
     console.log(`${index + 1}- ${Lista}`);
