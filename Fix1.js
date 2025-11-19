476f5333eee39bdbc0541949529390464cde8ae5
Christopher <chrissimao68@gmail.com>
2025-11-19 14:29:25 -0300

    exemplo lista compras

--- Fix1.js
+++ Fix1.js
@@ -1,6 +1,6 @@
 //Lista de compras, colocar no array e mostrar na tela
 let ListaCompras = []
-ListaCompras.push("Arroz", "Feijão", "Açucar", "Alcatra", "Maminha", "Fraudinha", "Óleo", "Queijo", "Alho", "Cebola")
+ListaCompras.push("Arroz", "Feijão", "Açucar", "Alcatra", "Maminha", "Fraudinha", "Óleo de Soja", "Queijo", "Alho", "Cebola")
 console.log(ListaCompras)
 
 ListaCompras.forEach((Lista, index) => {
